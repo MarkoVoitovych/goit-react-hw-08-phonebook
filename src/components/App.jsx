bfff1b8aa6649b02a2b9627e5aa65a00a09b4dcd
MarkoVoitovych <searchangeos@gmail.com>
2022-12-17 21:09:49 +0200

    default export added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,12 @@
 import { Component } from 'react';
 import { Container, MainTitle, Title } from './App.styled';
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
